e7d5b3649d4da2613afb7e06bd6c3e8cc606ddf4
João Andrade <andradepinheironeto24@hotmail.com>
2023-05-29 02:06:10 -0300

    criando componentização de formularios

--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -1,15 +1,16 @@
 interface InputProps {
   type: string;
-  placeholder?: string;
+  register: any;
+  id: string
 }
 
 export default function Input(props: InputProps) {
-  const { type, placeholder } = props;
+  const { type, id , register} = props;
   return (
     <input
       type={type}
+      {...register(id)}
       className="bg-gray-50 w-full h-11 rounded-xl p-2 border-gray-200 border-2 outline-blue-600"
-      placeholder={placeholder}
     />
   );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,8 +2,21 @@ import CompraSegura from "@/components/CompraSegura";
 import TopoTelaLoginECadastro from "@/components/TopoTelaLoginECadastro";
 import Input from "@/components/forms/Input";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 export default function index() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  function realizarLogin(data: any) {
+    console.log(data);
+  }
+
   return (
     <div className="h-screen flex flex-col space-y-5 items-center">
       <TopoTelaLoginECadastro />
@@ -13,22 +26,28 @@ export default function index() {
         </div>
 
         <div className="p-5">
-          <div className="space-y-3">
-            <Input type="email" />
-            <Input type="password" />
-          </div>
+          <form onSubmit={handleSubmit(realizarLogin)}>
+            <div className="space-y-3">
+              <Input type="email" id="email" register={register} />
+              <Input type="password" id="senha" register={register} />
+            </div>
 
-          <div className="flex flex-col items-center space-y-3 mt-3">
-            <button className="bg-btn-cor-verde rounded h-12 w-full md:w-72 text-white font-bold text-2xl">
-              Continuar
-            </button>
-            <Link
-              href="/cadastro"
-              className="text-xs text-blue-600 hover:underline"
-            >
-              Esqueceu sua senha? Clique aqui
-            </Link>
-          </div>
+            <div className="flex flex-col items-center space-y-3 mt-3">
+              <button
+                onClick={() => realizarLogin}
+                className="bg-btn-cor-verde rounded h-12 w-full md:w-72 text-white font-bold text-2xl"
+              >
+                Continuar
+              </button>
+              <Link
+                href="/cadastro"
+                className="text-xs text-blue-600 hover:underline"
+              >
+                Esqueceu sua senha? Clique aqui
+              </Link>
+            </div>
+          </form>
+
           <CompraSegura />
         </div>
       </div>
